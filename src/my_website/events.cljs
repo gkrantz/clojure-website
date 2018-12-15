b072f9850e92522961187a418be6e4c5bfaeda28
gkrantz <g-u-r-r-a@hotmail.com>
2018-12-15 02:04:27 +0100

    small refactoring

--- src/my_website/events.cljs
+++ src/my_website/events.cljs
@@ -1,9 +1,9 @@
 (ns my-website.events
-  (:require [my-website.js-interops :as js-interops]))
+  (:require [my-website.js-interops :refer [set-url-path]]))
 
 (def events
   {:rename-user (fn [state-atom {name :name}]
                   (swap! state-atom (fn [old-state] (assoc-in old-state [:user :name] name))))
    :route       (fn [state-atom {to :to _ :from}]
                   (swap! state-atom (fn [old-state] (assoc old-state :location to)))
-                  (js-interops/set-url-path to))})
+                  (set-url-path to))})
