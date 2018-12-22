4ae5861af76b50c8c1d7ec3d1b8f6c3781c7d3d9
gkrantz <g-u-r-r-a@hotmail.com>
2018-12-22 08:17:44 +0100

    a working view

--- src/tower_defence/constants.cljc
+++ src/tower_defence/constants.cljc
@@ -1,4 +1,4 @@
 (ns tower-defence.constants)
 
 (def SQUARE_SIZE 32)
-(def TICKS_PER_SECOND 10)
+(def TICKS_PER_SECOND 24)
--- src/tower_defence/view.cljs
+++ src/tower_defence/view.cljs
@@ -1,7 +1,12 @@
 (ns tower-defence.view
+  (:require-macros
+    [cljs.core.async.macros :refer [go go-loop alt!]])
   (:require [rum.core :as rum]
             [tower-defence.player-api :as game]
-            [tower-defence.constants :as constants]))
+            [tower-defence.constants :as constants]
+            [cljs.core.async :refer [close! put! chan <! timeout unique alts!]]))
+
+(def game-atom (atom (game/start-game)))
 
 (defn- get-cells
   [height width]
@@ -21,83 +26,73 @@
   [state]
   (vals (:monsters state)))
 
+(defn get-image
+  [path]
+  (let [img (js/Image.)]
+    (aset img "src" path)
+    img))
+
+(def image32x32 (get-image "images/tower-defence/32x32.png"))
+(def basic (get-image "images/tower-defence/basic.png"))
+(def blob (get-image "images/tower-defence/blob.png"))
+
+(defn draw-chan
+  []
+  (let [channel (chan)
+        request-anim #(.requestAnimationFrame js/window %)]
+    (letfn [(trigger-redraw []
+              (put! channel 1)
+              (request-anim trigger-redraw))]
+      (request-anim trigger-redraw)
+      channel)))
+
+(defn get-state!
+  []
+  (deref game-atom))
+
 (defn draw-background
-  [state size]
-  (map-indexed (fn [id [y x]]
-                 [:img {:key   id
-                        :src   (str "images/tower-defence/32x32.png")
-                        :style {:display          "inline-block"
-                                :z-index          0
-                                :position         "absolute"
-                                :transform        (str "translateY(" (* y size) "px)"
-                                                       "translateX(" (* x size) "px)")
-                                :height           (str size "px")
-                                :width            (str size "px")
-                                :background-color "black"}}])
-               (get-cells (:height state) (:width state))))
+  [state ctx]
+  (doseq [[y x] (get-cells (:height state) (:width state))]
+    (.drawImage ctx image32x32 (* x 32) (* y 32))))
 
 (defn draw-towers
-  [state size]
-  (map (fn [tower]
-         [:img {:key   (:id tower)
-                :src   (str "images/tower-defence/basic.png")
-                :style {:display          "inline-block"
-                        :z-index          1
-                        :position         "absolute"
-                        :transform        (str "translateY(" (* (first (:square tower)) size) "px)"
-                                               "translateX(" (* (second (:square tower)) size) "px)")
-                        :height           (str size "px")
-                        :width            (str size "px")
-                        :background-color "black"}}])
-       (get-towers state)))
+  [state ctx]
+  (doseq [tower (get-towers state)]
+    (.drawImage ctx basic (* (second (:square tower)) 32) (* (first (:square tower)) 32))))
 
 (defn draw-monsters
-  [state]
-  (map (fn [monster]
-         [:img {:key   (:id monster)
-                :src   (str "images/tower-defence/blob.png")
-                :style {:display          "inline-block"
-                        :z-index          2
-                        :position         "absolute"
-                        :transition       "0.1s"
-                        :transform        (str "translateY(" (- (:y monster) 9) "px)"
-                                               "translateX(" (- (:x monster) 9) "px)")
-                        :height           (str "18px")
-                        :width            (str "18px")
-                        :background-color "black"}}])
-       (get-monsters state)))
+  [state ctx]
+  (doseq [monster (get-monsters state)]
+    (.drawImage ctx blob (int (- (:x monster) 9)) (int (- (:x monster) 9)))))
 
-(defonce state-atom (atom nil))
+(defn draw-game
+  [state ctx]
+  (draw-background state ctx)
+  (draw-towers state ctx)
+  (draw-monsters state ctx))
 
-(rum/defc game-component
-  [state]
-  (let [size constants/SQUARE_SIZE]
-    [:div
-     {:key   "game-div"
-      :style {:position   "relative"
-              :text-align "left"
-              :height     (str (* (:height state) size) "px")
-              :width      (str (* (:width state) size) "px")}}
-     (draw-background state size)
-     (draw-towers state size)
-     (draw-monsters state)]))
-
-(rum/defc no-game-component
-  [state-atom]
-  [:div {:on-click (fn []
-                     (println @state-atom)
-                     (reset! state-atom (game/start-game)))}
-   "Press here to start game!"])
-
-(rum/defc component < rum/reactive
+(defn start-draw-loop!
   []
-  ;(println @state-atom)
-  [:div {:style {:width            "400px"
-                 :height           "400px"
-                 :background-color "green"}}
-   (if (nil? (rum/react state-atom)) (no-game-component state-atom)
-                          (game-component @state-atom))
-   [:button {:on-click (fn [] (swap! state-atom (fn [old] (game/tick old))))} "Tick"]
+  (let [redraw-chan (draw-chan)
+        ctx (as-> (.getElementById js/document "canvas0") $
+                  (.getContext $ "2d"))]
+    (go-loop [state nil]
+             (<! redraw-chan)
+             (let [new-state (get-state!)]
+               (when (not= state new-state)
+                 (draw-game new-state ctx))
+               (recur new-state)))))
+
+(rum/defc component
+  []
+  [:div
+   [:button {:on-click (fn [] (start-draw-loop!))} "Start Game!"]
+   [:button {:on-click (fn [] (reset! game-atom (game/tick @game-atom)))} "Tick!"]
    [:button {:on-click (fn [] (js/setInterval
-                                #(reset! state-atom (game/tick @state-atom))
-                                110))} "timer"]])
+                                #(reset! game-atom (game/tick @game-atom))
+                                (/ 1000 constants/TICKS_PER_SECOND)))} "Tick timer"]
+   [:canvas {:class  "tdgame"
+             :id     "canvas0"
+             :width  384
+             :height 384
+             :style  {:background-color "green"}}]])
